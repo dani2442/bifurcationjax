f254d1896a270aad74c7228be088778776ec3bf9
dani2442 <daniel.l.m.2442.123@gmail.com>
2024-07-06 00:13:33 +0200

    test predictors and correctors

--- tests/test_predictors.py
+++ tests/test_predictors.py
@@ -1,9 +1,9 @@
-"""
-Example 1 shown in https://bifurcationkit.github.io/BifurcationKitDocs.jl/stable/gettingstarted/
-"""
+"""Test Correctors."""
 
 import jax.numpy as jnp
 import jax
+
+import matplotlib.pyplot as plt
 
 from bifurcationjax.continuation.Corrector import CorrectorParams
 from bifurcationjax.continuation.Predictor import PredictorParams
@@ -27,11 +27,15 @@ def plot_fn(p):
 p0 = 0.
 x0 = jnp.array([-2.])
 
+dz0 = jnp.array([0.,1.])
+
 prob = BifurcationProblem(F, x0, p0,)
 par = ContinuationPar(p_min=-1., p_max=1., dsmax=0.1, max_steps=500)
-correction_params = CorrectorParams(method='PALC', epsilon=1e-3)
-prediction_params = PredictorParams(method='tangent', k=0)
-branches = continuation(prob, prediction_params, correction_params, par, max_depth=1)
 
-
-plot_bifurcation_diagram(branches, plot_fn=plot_fn, path_save='images/example1.png')
+fig, axs = plt.subplots(1, 2, figsize=(15, 6))
+for method, ax in zip(['tangent', 'secant'], axs.reshape(-1)):
+    correction_params = CorrectorParams(method='PALC', epsilon=1e-3)
+    prediction_params = PredictorParams(method=method, k=0, dz0=dz0)
+    diagram = continuation(prob, prediction_params, correction_params, par, max_depth=1)
+    plot_bifurcation_diagram(diagram, plot_fn=plot_fn, ax=ax, title=method)
+plt.show()
